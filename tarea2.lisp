b5c45236eadc41d412488275c6b3dcb56d741ace
Broski <ocorellaq@symbiotic.net>
2017-10-29 19:06:46 -0600

    Encripta working

--- tarea2.lisp
+++ tarea2.lisp
@@ -56,3 +56,47 @@
 		)
 	)
 )
+
+;;Encripta una hilera dada un alfabeto de entrada y otro de salida
+;;(encripta '(o s c a r a p u) '(a b c d e f g h i j k l m n o p q r s t u v w x y z) '(a b c d e f g h i j k l m n o p q r s t u 0 1 2 3 4 5 6 7 8 9))
+;; devuelve Hilera encriptada = (O S 7 0 H Q A F) Estado final = (U . F)
+
+(defun encripta (H Ae As)
+	(defparameter AlfabetoEntrada Ae)
+	(defparameter AlfabetoSalida As)
+	(defparameter Hilera H)
+	(defparameter Resultado '())
+	(recorrer (list-length H))
+	(write-line "")
+	(format nil "Hilera encriptada = ~S Estado final = ~S" Resultado (cons (car AlfabetoEntrada) (car AlfabetoSalida)))
+)
+
+;;Recorre la lista a encriptar
+(defun recorrer (cantidad)
+	(cond((> cantidad 0)
+		(girarLista (car Hilera))
+		(setq Hilera (rotarN Hilera 1))
+		(setq Resultado (append Resultado (list(car AlfabetoSalida))))
+		(recorrer (1- cantidad))
+		)
+	)
+)
+
+;;Rota hasta llegar al valor
+(defun girarLista (valor)
+	(cond((not (equal valor (car AlfabetoEntrada)))
+			(setq AlfabetoEntrada (rotarN AlfabetoEntrada 1))
+			(setq AlfabetoSalida (rotarN AlfabetoSalida 1))
+			(girarLista valor)
+		)
+	)
+)
+
+;;Rota la lista n veces
+(defun rotarN (lista n)
+	(cond((> n 0)
+			(rotarN (append (cdr lista) (list(car lista))) (1- n))
+		)
+		(t lista)
+	)
+)
